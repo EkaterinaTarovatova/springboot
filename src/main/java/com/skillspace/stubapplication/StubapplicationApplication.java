8f230a1054d4dc1f6f91b4c2017ec17c0618fda3
Ekaterina <caitlin.shoo@gmail.com>
2026-03-15 20:35:15 +0300

    Spring initialize

---
+++ src/main/java/com/skillspace/stubapplication/StubapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.skillspace.stubapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StubapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StubapplicationApplication.class, args);
+	}
+
+}
